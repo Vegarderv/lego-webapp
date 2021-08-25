d0de21cc8ed064720451e31a6b51329450922228
PeterJFB <peterj.flbj@gmail.com>
2021-08-25 21:42:06 +0200

    create new frontpage for logged in users

--- app/routes/overview/components/ArticleItem.js
+++ app/routes/overview/components/ArticleItem.js
@@ -22,30 +22,57 @@ class ArticleItem extends Component<Props, *> {
     const DESC_MAX_LENGTH = 230;
 
     return (
-      <div className={styles.body} style={weekly && { margin: '0' }}>
-        <Flex column>
-          <Flex column>
-            <Link to={url} className={styles.link}>
-              <Image className={styles.image} src={item.cover} />
-              <div className={styles.infoWrapper}>
-                <h2 className={styles.articleTitle}>
-                  {truncateString(item.title, TITLE_MAX_LENGTH)}
-                </h2>
-                <span className={styles.articleMeta}>
-                  Publisert - {meta.props.children[0]}
-                </span>
-                <div className={styles.articleMeta}>
-                  {meta.props.children[3]}
-                </div>
+      <Flex column className={styles.wrapper}>
+        <Link to={url} className={styles.link}>
+          <Flex className={styles.topWrapper}>
+            <div className={styles.titleAndTags}>
+              <div className={styles.title}>
+                {/* {truncateString(item.title, TITLE_MAX_LENGTH).toUpperCase()} */}
               </div>
-              <p className={styles.articleDescription}>
-                {truncateString(item.description, DESC_MAX_LENGTH)}
-              </p>
-            </Link>
+              {/* <Flex alignItems="center" className={styles.tags}>
+                {meta.props.children[3]}
+              </Flex> */}
+            </div>
+            <Image src={item.cover} />
           </Flex>
-        </Flex>
-      </div>
+          <Flex column className={styles.bottomWrapper}>
+            <div className={styles.title}>
+              {truncateString(item.title, TITLE_MAX_LENGTH).toUpperCase()}
+            </div>
+            <Flex alignItems="center" className={styles.tags}>
+              {meta.props.children[3]}
+            </Flex>
+            {truncateString(item.description, DESC_MAX_LENGTH)}
+          </Flex>
+        </Link>
+      </Flex>
     );
+
+    // return (
+    //   <div className={styles.body} style={weekly && { margin: '0' }}>
+    //     <Flex column>
+    //       <Flex column>
+    //         <Link to={url} className={styles.link}>
+    //           <Image className={styles.image} src={item.cover} />
+    //           <div className={styles.infoWrapper}>
+    //             <h2 className={styles.articleTitle}>
+    //               {truncateString(item.title, TITLE_MAX_LENGTH)}
+    //             </h2>
+    //             <span className={styles.articleMeta}>
+    //               Publisert - {meta.props.children[0]}
+    //             </span>
+    //             <div className={styles.articleMeta}>
+    //               {meta.props.children[3]}
+    //             </div>
+    //           </div>
+    //           <p className={styles.articleDescription}>
+    //             {truncateString(item.description, DESC_MAX_LENGTH)}
+    //           </p>
+    //         </Link>
+    //       </Flex>
+    //     </Flex>
+    //   </div>
+    // );
   }
 }
 
---
+++ app/routes/overview/components/WeeklyPlacard.js
@@ -0,0 +1,64 @@
+// @flow
+
+import type { Article } from 'app/models';
+
+import moment from 'moment-timezone';
+
+import Flex from 'app/components/Layout/Flex';
+import PlacardButton from './PlacardButton';
+
+import styles from './WeeklyPlacard.css';
+
+import weeklyLogo from 'app/assets/weekly_logo.svg';
+import weeklyMug from 'app/assets/weekly_mug.svg';
+
+import { Image } from 'app/components/Image';
+import Placard from './Placard';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  item: Article,
+  url: String,
+};
+
+const WeeklyPlacard = ({ item, url }: Props) => {
+  const WeeklyMug = ({ week }) => {
+    return (
+      <div className={styles.mug}>
+        <Image src={weeklyMug} alt={week}></Image>
+        <div>{week}</div>
+      </div>
+    );
+  };
+
+  const NewTag = () => <div className={styles.newTag}>NY</div>;
+
+  const datePosted = moment(item ? item.createdAt : Date.now());
+  const isNew = moment().diff(datePosted, 'day') < 5;
+  // Subtract a few days in case weekly is out on monday next week
+  const datePostedSafe = datePosted.subtract(2, 'day');
+  return (
+    <Flex
+      column
+      justifyContent="space-between"
+      alignItems="center"
+      className={styles.container}
+    >
+      <Placard>
+        <Link to="/articles?tag=weekly">
+          <Flex alignItems="flex-start" className={styles.logoAndMug}>
+            <Image src={weeklyLogo} alt="#WEEKLY" className={styles.logo} />
+            {isNew && <NewTag />}
+            <WeeklyMug week={datePostedSafe.week()} />
+          </Flex>
+        </Link>
+        <div className={styles.description}>
+          Se hva som har skjedd i Abakus denne uken, og hva som skjer fremover!
+        </div>
+        <PlacardButton to={url}>LES NYESTE</PlacardButton>
+      </Placard>
+    </Flex>
+  );
+};
+
+export default WeeklyPlacard;
